a095d6238c5edbb8646029eee641a204cd991638
Wisnu Hidayat <wisnubaldas@gmail.com>
2023-01-10 23:38:58 +0700

    delete ci3

--- application/modules/transhipment/controllers/Cpos_destination.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Cpos_destination extends CI_Controller
-{
-	
-	
-}
--- application/modules/transhipment/controllers/Cpostcode.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Cpostcode extends CI_Controller
-{
-	
-	
-}
