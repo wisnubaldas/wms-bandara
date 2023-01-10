80b29d4efd2102c2c347c5e303afffbb9cdbd018
Wisnu Hidayat <wisnubaldas@gmail.com>
2023-01-10 23:32:42 +0700

    ini versi 2 api ctos

--- application/modules/transhipment/controllers/Cpos_cn31.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Cpos_cn31 extends CI_Controller
-{
-	
-	
-}
--- application/modules/transhipment/controllers/Csol.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Csol extends CI_Controller
-{
-	
-	
-}
