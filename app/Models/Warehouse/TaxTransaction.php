bda9976656f16b336b0362f6c39bfdd33f98f007
Wisnu Hidayat <wisnubaldas@gmail.com>
2023-03-09 17:43:54 +0700

    finance model

---
+++ app/Models/Warehouse/TaxTransaction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Warehouse;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaxTransaction extends Model
+{
+    use HasFactory;
+    protected $connection= 'rdwarehouse_jkt';
+    protected $table = 'tax_transaction';
+    protected $primaryKey = 'noid';
+    // public $incrementing = false;
+    // protected $keyType = 'string';
+    public $timestamps = false;
+}
--- routes/web/ctos.php
+++ routes/web/ctos.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 // sesuaikan dulu kontroller nya
 use App\Http\Controllers\Ctos\ContLoginController;
 use App\Http\Controllers\Ctos\ContCloudStatusController;
+use App\Http\Controllers\Ctos\ContFinanceController;
 
 
 Route::middleware('auth:sanctum')->group(function()
@@ -28,6 +29,25 @@ Route::middleware('auth:sanctum')->group(function()
             Route::get('/get_total_inbound_delivery_aircarft/{id_header}/{status_date}/{status_time}/{airline_code}/{flight_no}/{origin}/{dest}', 'get_total_inbound_delivery_aircarft');
             Route::get('/get_check_th_inbound_international/{waybill_smu}/{hawb?}', 'get_check_th_inbound_international');
             Route::get('/get_check_th_outbond_international/{waybill_smu}/{hawb?}', 'get_check_th_outbond_international');
+        });
+    });
+    Route::prefix('Cont_finance')->group(function () {
+        Route::controller(ContFinanceController::class)->group(function () {
+            Route::get('/get_drsc_import/{shifname}/{dateDRSC}/{PaymentCode}/{token}', 'get_drsc_import');
+            Route::get('/get_drsc_ekspor/{shifname}/{dateDRSC}/{PaymentCode}/{token}', 'get_drsc_ekspor');
+            Route::get('/get_list_basicprice/{LocationCode}/{WarehouseCode}/{AgreementCode}/{Datetransaction}/{totOverstay}', 'get_list_basicprice');
+            Route::get('/get_list_aggrement/{WarehouseCode}/{DatabaseCode}{kd_gudang}', 'get_list_aggrement');
+            Route::get('/list_fare_directory/{WarehouseCode}/{AgreementCode}', 'list_fare_directory');
+            Route::get('/get_list_pajakBynumber/{token}/{TaxAwal}/{TaxAkhir}', 'get_list_pajakBynumber');
+            Route::get('/get_list_pajakBydate/{token}/{DateFrom}/{DateUntil}', 'get_list_pajakBydate');
+            Route::get('/get_totalfaktur/{warehouse_npwp}', 'get_totalfaktur');
+            Route::get('/get_deposit_invoice/{InvoiceNumber}', 'get_deposit_invoice');
+            Route::get('/get_list_depositheader/{DepositCode}', 'get_list_depositheader');
+            Route::get('/get_list_depositdetail/{WarehouseCode}/{DepositCode}/{firstdate}/{lastdate}', 'get_list_depositdetail');
+            Route::get('/get_Summary_deposit/{WarehouseCode}/{DepositCode}/{firstdate}', 'get_Summary_deposit');
+            Route::get('/get_list_depositnominal/{DepositCode}/{WarehouseCode?}', 'get_list_depositnominal');
+            Route::get('/get_list_depositor/{udepositor?}', 'get_list_depositor');
+            Route::get('/get_nomor_tax/{warehouse_npwp}', 'get_nomor_tax');
         });
     });
 
