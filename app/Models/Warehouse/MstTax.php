021d22034e31ab37aa1d101caad0deb793e9826a
wisnuhidayat <wisnubaldas@gmail.com>
2023-03-10 16:39:18 +0700

    masih tax

---
+++ app/Models/Warehouse/MstTax.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Warehouse;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MstTax extends Model
+{
+    use HasFactory;
+    protected $connection= 'rdwarehouse_jkt';
+    protected $table = 'mst_tax';
+    protected $primaryKey = 'noid';
+    // public $incrementing = false;
+    // protected $keyType = 'string';
+    // public $timestamps = false;
+    // const CREATED_AT = 'created_at';
+    // const UPDATED_AT = 'modify_at'; 
+}
